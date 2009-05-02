c289046965e17b623b1f1de802283d1584a33af9
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2009-05-02 20:02:37 +0200

    signaltest: fix typo in copyright header
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>

--- src/signaltest/signaltest.c
+++ src/signaltest/signaltest.c
@@ -4,7 +4,7 @@
  * (C) 2007 Thomas Gleixner <tglx@linutronix.de>
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License Veriosn
+ * modify it under the terms of the GNU General Public License Version
  * 2 as published by the Free Software Foundation;
  *
  */
