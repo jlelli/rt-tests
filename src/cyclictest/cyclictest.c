f02c5d5ad010ba3a5bdf54d400bbe4b7a81101bd
Carsten Emde <C.Emde@osadl.org>
2010-03-07 21:39:57 +0100

    remove-incorrect-options-from-smp-help-message-in-cyclictest.patch
    The help message of cyclictest's -S option says that it equals -a -t -n -m -d0.
    In reality, it only equals -a -t -n.
    
    Signed-off-by: Carsten Emde <C.Emde@osadl.org>
    Acked-by: John Kacur <jkacur@redhat.com>

--- src/cyclictest/cyclictest.c
+++ src/cyclictest/cyclictest.c
@@ -778,8 +778,8 @@ static void display_help(int error)
                "-W       --wakeuprt        rt task wakeup tracing (used with -b)\n"
                "-y POLI  --policy=POLI     policy of realtime thread (1:FIFO, 2:RR)\n"
                "                           format: --policy=fifo(default) or --policy=rr\n"
-	       "-S       --smp             Standard SMP testing (equals -a -t -n -m -d0)\n"
-               "                           same priority on all threads.\n"
+	       "-S       --smp             Standard SMP testing: options -a -t -n and\n"
+               "                           same priority of all threads\n"
 	       "-U       --numa            Standard NUMA testing (similar to SMP option)\n"
                "                           thread data structures allocated from local node\n",
 	       tracers
