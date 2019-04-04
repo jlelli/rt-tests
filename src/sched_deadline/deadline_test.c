9bd019bd220a601a7948ce532e4f4eca7a7b6a97
Kurt Kanzenbach <kurt@linutronix.de>
2019-04-04 15:48:13 +0200

    rt-tests: deadline_tests: fix stack buffer flow
    The appended string is actually longer than 14 characters. Use strlen() to
    compute the correct length like the other functions do.
    
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
    Signed-off-by: John Kacur <jkacur@redhat.com>

--- src/sched_deadline/deadline_test.c
+++ src/sched_deadline/deadline_test.c
@@ -435,7 +435,7 @@ static void setup_ftrace_marker(void)
 {
 	struct stat st;
 	const char *debugfs = find_debugfs();
-	char files[strlen(debugfs) + 14];
+	char files[strlen(debugfs) + strlen("/tracing/trace_marker") + 1];
 	int ret;
 
 	if (strlen(debugfs) == 0)
