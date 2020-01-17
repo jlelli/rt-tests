c7a8dd7f01ac5d89ecb808ea803732f02466fec7
John Kacur <jkacur@redhat.com>
2020-01-17 18:19:19 +0100

    rt-tests: queuelat: get_cpuinfo_mhz.sh highest value
    get_cpuinfo_mhz.sh greps for the Mhz from the /proc/cpuinfo file
    It assumes that for multiple cpus the value will be the same, and
    intends to return one value using uniq as a filter.
    
    Typically the reported Mhz values can all be slightly different though
    It would probably be good enough to simply take the first match as a
    heuristic, but to be safe, take the highest number.
    
    Also replace the legacy backticks with the modern $(...) construct, and
    quote the $mhz variable.
    
    Signed-off-by: John Kacur <jkacur@redhat.com>

--- src/queuelat/get_cpuinfo_mhz.sh
+++ src/queuelat/get_cpuinfo_mhz.sh
@@ -3,6 +3,5 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2018 Marcelo Tosatti <mtosatti@redhat.com>
 
-mhz=`cat /proc/cpuinfo  | grep "cpu MHz" | uniq | cut -f 3 -d " "`
-echo $mhz
-
+mhz=$(grep "cpu MHz" /proc/cpuinfo | cut -f 3 -d " " | sort -rn | head -n1)
+echo "$mhz"
